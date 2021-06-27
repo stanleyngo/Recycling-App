73b82df1db77d28166d8d764b6b6f6338fc168d4
stanleyngo <56895786+stanleyngo@users.noreply.github.com>
2021-06-26 21:23:10 -0400

    Information Page (Missing Bottom Border)

---
+++ ContentView.swift
@@ -0,0 +1,108 @@
+//
+//  ContentView.swift
+//  Information Page
+//
+//  Created by Stanley Ngo on 6/26/21.
+//
+
+import SwiftUI
+
+let columns: [GridItem] = [GridItem(.flexible()),
+                           GridItem(.flexible()),]
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView{
+            VStack{
+                CircleView().offset(y: -90)
+            }
+        }
+    }
+}
+
+struct RecycleView: View {
+    var body: some View {
+        
+        
+        ZStack {
+            VStack{
+                Text("Information").font(.title).padding(.trailing, 150)
+                Spacer().frame(height:700)
+            }
+        }
+    }
+}
+
+struct CircleView: View {
+    var body: some View {
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                LazyVGrid(columns: columns, spacing: 0) {
+                    ForEach(0..<4) { i in
+                        ZStack {
+                            Circle()
+                                .foregroundColor(Color(red:150/255, green: 226/255, blue:165/255))
+                                .frame(width: geometry.size.width/3 - 15,
+                                       height: geometry.size.width - 30)
+                        }
+                    }
+                }
+            }
+            VStack {
+                Text("Why and How").padding(.top, 275).padding(.leading, 50)
+                Image(systemName: "questionmark").resizable().offset(y: -135).offset(x:13).foregroundColor(.white)
+                    .frame(width: 20, height: 40)
+            }
+            VStack {
+                Text("Effects and Impacts").padding(.top, 275).padding(.leading, 260)
+                Image(systemName: "exclamationmark").resizable()
+                    .offset(y: -160)
+                    .offset(x: 120)
+                    .frame(width: 15, height: 40)
+                    .foregroundColor(.white)
+            }
+            VStack {
+                Text("Statistics").padding(.top, 630).padding(.leading, 60)
+                Image(systemName:"chart.bar").resizable()
+                    .offset(y: -125)
+                    .offset(x: 27)
+                    .frame(width:28, height: 18)
+                    .foregroundColor(.white)
+            }
+            VStack {
+                Text("Types").padding(.top, 630).padding(.leading, 270)
+                Image(systemName:"trash")
+                    .resizable()
+                    .offset(y: -125)
+                    .offset(x: 135)
+                    .frame(width: 20, height: 30)
+                    .foregroundColor(.white)
+            }
+            /*VStack{
+                Spacer()
+                Rectangle()
+                    .frame(height: 50)
+                    .foregroundColor(Color(red: 172/255, green: 242/255, blue: 186/255))
+                    .edgesIgnoringSafeArea(.bottom)
+            }
+            .edgesIgnoringSafeArea(.bottom)
+                 */
+            /*VStack {
+                Spacer().frame(height: 100)
+                Image(systemName:"person.crop.circle.fill").resizable().frame(width:40, height:40).foregroundColor(.white)
+             */
+            }
+        }
+    }
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+            ContentView()
+        }
+    }
+}
