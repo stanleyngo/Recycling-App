6d6c7d7678c73ceb3dcfda13bd681072a7ff09aa
stanleyngo <56895786+stanleyngo@users.noreply.github.com>
2021-06-26 21:53:21 -0400

    Update Information Page

--- InformationPage.swift
+++ InformationPage.swift
@@ -15,6 +15,18 @@ struct ContentView: View {
         NavigationView{
             VStack{
                 CircleView().offset(y: -90)
+                VStack{
+                    Spacer()
+                    Rectangle()
+                        .frame(height: 75)
+                        .foregroundColor(Color(red: 172/255, green: 242/255, blue: 186/255))
+                }
+               /*VStack {
+                    Spacer()
+                    Image(systemName:"person.crop.circle.fill").resizable().frame(width:40, height:40)
+                }
+                */
+                .edgesIgnoringSafeArea(.all)
             }
         }
     }
@@ -51,50 +63,37 @@ struct CircleView: View {
             }
             VStack {
                 Text("Why and How").padding(.top, 275).padding(.leading, 50)
-                Image(systemName: "questionmark").resizable().offset(y: -135).offset(x:13).foregroundColor(.white)
+                Image(systemName: "questionmark").resizable().offset(y: -135).offset(x:15).foregroundColor(.white)
                     .frame(width: 20, height: 40)
             }
             VStack {
-                Text("Effects and Impacts").padding(.top, 275).padding(.leading, 260)
+                Text("Effects and Impacts").padding(.top, 275).padding(.leading, 220)
                 Image(systemName: "exclamationmark").resizable()
-                    .offset(y: -160)
-                    .offset(x: 120)
+                    .offset(y: -140)
+                    .offset(x: 110)
                     .frame(width: 15, height: 40)
                     .foregroundColor(.white)
             }
             VStack {
                 Text("Statistics").padding(.top, 630).padding(.leading, 60)
                 Image(systemName:"chart.bar").resizable()
-                    .offset(y: -125)
-                    .offset(x: 27)
-                    .frame(width:28, height: 18)
+                    .offset(y: -130)
+                    .offset(x: 29)
+                    .frame(width:40, height: 30)
                     .foregroundColor(.white)
             }
             VStack {
                 Text("Types").padding(.top, 630).padding(.leading, 270)
                 Image(systemName:"trash")
                     .resizable()
-                    .offset(y: -125)
+                    .offset(y: -135)
                     .offset(x: 135)
-                    .frame(width: 20, height: 30)
+                    .frame(width: 30, height: 40)
                     .foregroundColor(.white)
-            }
-            /*VStack{
-                Spacer()
-                Rectangle()
-                    .frame(height: 50)
-                    .foregroundColor(Color(red: 172/255, green: 242/255, blue: 186/255))
-                    .edgesIgnoringSafeArea(.bottom)
-            }
-            .edgesIgnoringSafeArea(.bottom)
-                 */
-            /*VStack {
-                Spacer().frame(height: 100)
-                Image(systemName:"person.crop.circle.fill").resizable().frame(width:40, height:40).foregroundColor(.white)
-             */
             }
         }
     }
+}
 
 
 
